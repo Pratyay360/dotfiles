2eddb6ab14164e5f7ab1d385ec863fedd1f67e30
Pratyay360 <pratyaymustafi@outlook.com>
2026-08-08 00:29:23 +0530

    Add .config/fish/conf.d/completion.fish

--- dot_config/private_fish/conf.d/completion.fish
+++
@@ -1,9 +0,0 @@
-buildah completion fish | source
-doppler completion fish | source
-#zrok2 completion fish | source
-#pixi completion --shell fish | source
-
-starship init fish | source
-zoxide init fish | source
-turso completion fish | source
-mise completion fish | source
