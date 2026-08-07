aa7774694259ad4c564253ae02e2509bc5d8f01b
Pratyay360 <pratyaymustafi@outlook.com>
2026-08-08 00:27:58 +0530

    12

---
+++ dot_config/private_fish/conf.d/aliass.fish
@@ -0,0 +1,15 @@
+alias aliass "vi $HOME/.config/fish/conf.d/aliass.fish"
+alias genv "vi $HOME/.config/vars"
+alias fishcompletions "vi $HOME/.config/fish/conf.d/completion.fish"
+alias ls "eza  --icons=always"
+alias cd z
+# alias npm "bun"
+# alias npx "bunx"
+# alias node "bun"
+alias cat bat
+alias uvsh "source .venv/bin/activate.fish"
+alias dig doggo
+alias vim nvim
+#alias docker "podman"
+#alias git "grit"
+#alias docker "nerdctl"
