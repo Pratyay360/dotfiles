24778cc0b01ad7e18abf08fd8c3e53aaec5387ce
Pratyay360 <pratyaymustafi@outlook.com>
2026-08-08 23:36:52 +0530

    Add .config/fish/conf.d/abbr_tips.fish Update .config/fish/conf.d/completion.fish Add .config/fish/conf.d/fish_frozen_key_bindings.fish Add .config/fish/conf.d/fzf.fish Add .config/fish/conf.d/fzf_configure_bindings.fish Add .config/fish/config.fish.bak.1781695502 Add .config/fish/fish_plugins Add .config/fish/functions/__abbr_tips_bind_newline.fish Add .config/fish/functions/__abbr_tips_bind_space.fish Add .config/fish/functions/__abbr_tips_clean.fish Add .config/fish/functions/__abbr_tips_init.fish Add .config/fish/functions/_fzf_configure_bindings_help.fish Add .config/fish/functions/_fzf_extract_var_info.fish Add .config/fish/functions/_fzf_preview_changed_file.fish Add .config/fish/functions/_fzf_preview_file.fish Add .config/fish/functions/_fzf_report_diff_type.fish Add .config/fish/functions/_fzf_report_file_type.fish Add .config/fish/functions/_fzf_search_directory.fish Add .config/fish/functions/_fzf_search_git_log.fish Add .config/fish/functions/_fzf_search_git_status.fish Add .config/fish/functions/_fzf_search_history.fish Add .config/fish/functions/_fzf_search_processes.fish Add .config/fish/functions/_fzf_search_variables.fish Add .config/fish/functions/_fzf_wrapper.fish Add .config/fish/functions/fisher.fish Add .config/fish/functions/fzf_configure_bindings.fish Add .config/fish/functions/getopts.fish Add .config/fish/functions/spark.fish Add .config/fish/themes/.keep

--- dot_config/private_fish/conf.d/completion.fish
+++ dot_config/private_fish/conf.d/completion.fish
@@ -1,12 +1,9 @@
-#buildah completion fish | source
+buildah completion fish | source
 doppler completion fish | source
-#zrok2 completion fish | source
+zrok2 completion fish | source
 #pixi completion --shell fish | source
 
-# starship init fish | source
+starship init fish | source
 zoxide init fish | source
-#    bkt completion fish | source
 turso completion fish | source
-#mise completion fish | source
-#mise completion fish | source
-##zb completion fish | source
+mise completion fish | source
